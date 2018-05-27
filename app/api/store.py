b10bcb9c1021216dc5d3d244372a2ff3de74a6e3
Lech Gudalewicz <lech@aiki.io>
2018-05-27 12:57:25 -0700

    jsonschema

--- app/api/store.py
+++ app/api/store.py
@@ -1,6 +1,48 @@
 from flask.views import MethodView
 from app.api import bp
 
+SCHEMA = {
+    'type': 'object',
+    'properties': {
+        'neighborhood': {'type': 'string'},
+        'street_adress': {'type': 'string'},
+        'city': {'type': 'string'},
+        'state': {'type': 'string', 'pattern': '^[A-Z]{2}$'},
+        'phone': {'type': 'string', 'pattern': '^[0-9]{3}-[0-9]{3}-[0-9]{4}$'},
+    },
+    'required': [
+        'neighborhood',
+        'street_adress',
+        'city', 'state',
+        'zip',
+        'phone'
+    ]
+}
+
+
+def store_obj(doc):
+    return {
+        'id': doc.external_id,
+        'neighborhood': doc.neighborhood,
+        'street_adress': doc.street_adress,
+        'city': doc.city,
+        'state': doc.state,
+        'zip': doc.zip,
+        'phone': doc.phone,
+        'links': [
+            {
+                'rel': 'self',
+                'href': '/api/stores/' + doc.external_id
+            }
+
+        ]
+    }
+
+
+def stores_obj(stores):
+    stores = []
+    return stores
+
 
 class StoreApi(MethodView):
     pass
